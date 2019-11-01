eb05c3f812131d1ea9fb3aee80f9cf4c9d04226b
xuanli328 <xuan.3.li@uconn.edu>
2019-10-31 21:02:44 -0400

    add some constants fetched from database

--- src/main/java/SuperDelivery/service/idm/constants/DeliveryServiceInfo.java
+++ src/main/java/SuperDelivery/service/idm/constants/DeliveryServiceInfo.java
@@ -14,11 +14,20 @@ public class DeliveryServiceInfo {
     public static final double ROBOTSPEED = 1.4;  // unit: m/s
     // DJI Inspire 2 can fly at 58mph (25.9m/s) at top speed
     public static final double DRONESPEED = 22.5; // unit: m/s
-    public static final double ROBOTCOST = 0.001; // unit: dollar/m
-    public static final double DRONECOST = 0.008; // unit: dollar/m
+    // Cost related to distance
+    public static final double ROBOTCOST_DIST = 0.001; // unit: dollar/m
+    public static final double DRONECOST_DIST = 0.008; // unit: dollar/m
+    // TODO: add cost related to size and weight
+    public static int ROBOT_TYPEID;
+    public static int DRONE_TYPEID;
+
     public static LocationLatLon WAREHOUSE1;
     public static LocationLatLon WAREHOUSE2;
     public static LocationLatLon WAREHOUSE3;
+
+    public static int ORDER_PLACED;
+    public static int IN_PROGRESS;
+    public static int DELIVERED;
 
     static {
         try {
@@ -42,10 +51,44 @@ public class DeliveryServiceInfo {
                         ServiceLogger.LOGGER.warning("Database (table: warehouse_info) has more than 3 warehouses, but only used 3 warehouses here!");
                 }
             }
+
+            sql = "SELECT * FROM delivery_type ORDER BY deliveryTypeID";
+            statement = IDMService.getCon().createStatement();
+            rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                switch (rs.getString("deliveryType")) {
+                    case "ROBOT":
+                        ROBOT_TYPEID = rs.getInt("deliveryTypeID");
+                        break;
+                    case "DRONE":
+                        DRONE_TYPEID = rs.getInt("deliveryTypeID");
+                        break;
+                    default:
+                        ServiceLogger.LOGGER.warning("Database (table: delivery_type) has more than 2 worker types, but only used 2 worker types here!");
+                }
+            }
+
+            sql = "SELECT * FROM delivery_status ORDER BY deliveryStatusID";
+            statement = IDMService.getCon().createStatement();
+            rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                switch (rs.getString("deliveryStatus")) {
+                    case "Order Placed":
+                        ORDER_PLACED = rs.getInt("deliveryStatusID");
+                        break;
+                    case "In Progress":
+                        IN_PROGRESS = rs.getInt("deliveryStatusID");
+                        break;
+                    case "Delivered":
+                        DELIVERED = rs.getInt("deliveryStatusID");
+                        break;
+                    default:
+                        ServiceLogger.LOGGER.warning("Database (table: delivery_status) has more than 3 status, but only used 3 status here!");
+                }
+            }
         } catch (SQLException e) {
             ServiceLogger.LOGGER.warning("Error during query.");
             e.printStackTrace();
         }
     }
-
 }
--- src/main/java/SuperDelivery/service/idm/models/WorkerType.java
+++ src/main/java/SuperDelivery/service/idm/models/WorkerType.java
@@ -1,22 +1,42 @@
 package SuperDelivery.service.idm.models;
 
+import static SuperDelivery.service.idm.constants.DeliveryServiceInfo.*;
+
 public enum WorkerType {
-    ROBOT ("ROBOT", false),
-    DRONE ("DRONE", true);
+    ROBOT ("ROBOT", ROBOT_TYPEID, false, ROBOTSPEED, ROBOTCOST_DIST),
+    DRONE ("DRONE", DRONE_TYPEID, true, DRONESPEED, DRONECOST_DIST);
 
     private final String dbName;
+    private final int typeID;
     private final boolean isGeodesic;
+    private final double speed;
+    private final double costDist;
 
-    WorkerType(String dbName, boolean isGeodesic) {
+    WorkerType(String dbName, int typeID, boolean isGeodesic, double speed, double costDist) {
         this.dbName = dbName;
+        this.typeID = typeID;
         this.isGeodesic = isGeodesic;
+        this.speed = speed;
+        this.costDist = costDist;
     }
 
     public String getDbName() {
         return dbName;
     }
 
+    public int getTypeID() {
+        return typeID;
+    }
+
     public boolean isGeodesic() {
         return isGeodesic;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getCostDist() {
+        return costDist;
     }
 }
